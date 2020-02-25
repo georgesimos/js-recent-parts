dc348b35a3bb7a0f3e8b1177c503ccaa76028101
George <admin@georgesimos.com>
2020-02-25 19:28:48 +0200

    array destructure side by side...

---
+++ arrayDestructuring/first.js
@@ -0,0 +1,11 @@
+function data() {
+  return [1, 2, 3];
+}
+
+var temp = data();
+
+var first = temp[0];
+var second = temp[1];
+var third = temp[2];
+
+// #####################
---
+++ arrayDestructuring/second.js
@@ -0,0 +1,7 @@
+function data() {
+  return [1, 2, 3];
+}
+
+var [first, second, third] = data();
+
+// #####################
