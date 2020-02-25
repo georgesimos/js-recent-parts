b9f4c39cdb36927d775c7b83632ed9bcd9080553
George <admin@georgesimos.com>
2020-02-25 19:04:44 +0200

    UpperCase the values of literal tags

---
+++ strings/taggedTemplateEx.js
@@ -0,0 +1,17 @@
+function upper(strings, ...values) {
+  var str = "";
+  for (let i = 0; i < strings.length; i++) {
+    if (i > 0) {
+      str += values[i - 1].toUpperCase();
+    }
+    str += strings[i];
+  }
+  return str;
+}
+
+var name = "George",
+  twitter = "simos",
+  topic = "JS Recent Parts";
+
+const msg = upper`Hello ${name} (@${twitter}), welcome to ${topic}`;
+console.log(msg === `Hello GEORGE (@SIMOS), welcome to JS RECENT PARTS`);
