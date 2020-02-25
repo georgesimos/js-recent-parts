45ed939ced832901fbfd956608d89d1f435b4694
George <admin@georgesimos.com>
2020-02-25 19:33:03 +0200

    set default values if array have element === undefined

--- arrayDestructuring/first.js
+++ arrayDestructuring/first.js
@@ -2,10 +2,10 @@ function data() {
   return [1, 2, 3];
 }
 
-var temp = data();
+var tmp = data();
 
-var first = temp[0];
-var second = temp[1];
-var third = temp[2];
+var first = tmp[0];
+var second = tmp[1] !== undefined ? tmp[1] : 2;
+var third = tmp[2];
 
 // #####################
--- arrayDestructuring/second.js
+++ arrayDestructuring/second.js
@@ -2,6 +2,6 @@ function data() {
   return [1, 2, 3];
 }
 
-var [first, second, third] = data();
+var [first, second = 2, third] = data();
 
 // #####################
