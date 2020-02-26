b65f23a2c6057d82f89d4662994fe629f3053248
George <admin@georgesimos.com>
2020-02-26 12:32:52 +0200

    ...

--- arrayDestructuring/second.js
+++ arrayDestructuring/second.js
@@ -1,7 +1,9 @@
 function data() {
   return [1, 2, 3, 4, 5];
 }
+var tmp = data();
 var first, second, thrird, rest;
-[first, second = 2, third, ...rest] = data();
+[first, second = 2, third, ...rest] = tmp;
 console.log(rest);
+
 // #####################
