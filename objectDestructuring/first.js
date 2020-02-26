dac7564824a0debd20971248b2ace86b7532829c
George <admin@georgesimos.com>
2020-02-26 13:21:50 +0200

    nested obbject destructuring

--- objectDestructuring/first.js
+++ objectDestructuring/first.js
@@ -1,10 +1,17 @@
 function data() {
-  return { a: 1, b: 2, c: 3, d: 4 };
+  return {
+    a: 1,
+    b: {
+      c: 3,
+      d: 4
+    }
+  };
 }
 
 var tmp = data() || {};
 var first = tmp.a !== undefined ? tmp.a : 42;
-var second = tmp.b;
-var third = tmp.c;
+var tmp2 = tmp.b || {};
+var second = tmp2.c;
+var third = tmp2.d;
 
 console.log(first, second, third);
--- objectDestructuring/second.js
+++ objectDestructuring/second.js
@@ -1,6 +1,12 @@
 function data() {
-  return { a: 1, b: 2, c: 3, d: 4 };
+  return {
+    a: 1,
+    b: {
+      c: 3,
+      d: 4
+    }
+  };
 }
 
-var { a: first = 42, b: second, ...rest } = data() || {};
-console.log(first, second, rest);
+var { a: first = 42, b: { c: third, d: fourth } = {}, ...rest } = data() || {};
+console.log(first, third, fourth, rest);
