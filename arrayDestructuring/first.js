cc70c9e46aec881992a70ec389eb85c173b518d5
George <admin@georgesimos.com>
2020-02-26 12:52:05 +0200

    Nested Arrays Destructuring

--- arrayDestructuring/first.js
+++ arrayDestructuring/first.js
@@ -1,32 +1,49 @@
-function data() {
-  return [1, 2, 3, 4, 5];
-}
+// function data() {
+//   return [1, 2, 3, 4, 5];
+// }
 
-var tmp = data() || [];
-var first, second, third, rest;
-first = tmp[0];
+// var tmp = data() || [];
+// var first, second, third, rest;
+// first = tmp[0];
 // second = tmp[1] !== undefined ? tmp[1] : 2;
-third = tmp[2];
-rest = tmp.slice(3);
-console.log(first, second, third, rest);
+// third = tmp[2];
+// rest = tmp.slice(3);
+// console.log(first, second, third, rest);
+
+// // #####################
+
+// // Swap
+// var x = 10;
+// var y = 20;
+// console.log(x, y);
+// {
+//   let temp = x;
+//   x = y;
+//   y = temp;
+// }
+// console.log(x, y);
+
+// // #####################
+// // Parameter Arrays
+// function data2(tmp = []) {
+//   var [first = 10, second = 20, third = 30] = tmp;
+//   console.log(first, second, third);
+// }
+// data2();
 
 // #####################
-
-// Swap
-var x = 10;
-var y = 20;
-console.log(x, y);
-{
-  let temp = x;
-  x = y;
-  y = temp;
+// Nested Arrays Destructuring
+function data3() {
+  return [1, [2, 3], 4, 5];
 }
-console.log(x, y);
 
+var tmp = data3() || [];
+
+var first = tmp[0];
+var tmp2 = tmp[1] || [];
+var second = tmp2[0];
+var third = tmp2[1];
+var fourth = tmp[2];
+var rest = tmp.slice(3);
+console.log(first, second, third, fourth, rest);
 // #####################
-// Parameter Arrays
-function data2(tmp = []) {
-  var [first = 10, second = 20, third = 30] = tmp;
-  console.log(first, second, third);
-}
-data2();
--- arrayDestructuring/second.js
+++ arrayDestructuring/second.js
@@ -1,23 +1,36 @@
-function data() {
-  return [1, 2, 3, 4, 5];
-}
-var tmp = data() || [];
-var first, second, third, rest;
-[first, , third, ...rest] = tmp;
-console.log(first, second, third, rest);
+// function data() {
+//   return [1, 2, 3, 4, 5];
+// }
+// var tmp = data() || [];
+// var first, second, third, rest;
+// [first, , third, ...rest] = tmp;
+// console.log(first, second, third, rest);
+
+// // #####################
+
+// // Swap
+// var x = 10;
+// var y = 20;
+// console.log(x, y);
+// [y, x] = [x, y];
+// console.log(x, y);
+
+// // #####################
+// // Parameter Arrays
+// function data2([first = 10, second = 20, third = 30] = []) {
+//   console.log(first, second, third);
+// }
+// data2();
 
 // #####################
-
-// Swap
-var x = 10;
-var y = 20;
-console.log(x, y);
-[y, x] = [x, y];
-console.log(x, y);
+// Nested Arrays Destructuring
+function data3() {
+  return [1, [2, 3], 4, 5];
+  //    return [1, undefined, 4, 5];
+}
+var tmp = data3() || [];
+var first, second, third, fourth, rest;
+[first, [second, third] = [], fourth, ...rest] = tmp;
+console.log(first, second, third, fourth, rest);
 
 // #####################
-// Parameter Arrays
-function data2([first = 10, second = 20, third = 30] = []) {
-  console.log(first, second, third);
-}
-data2();
