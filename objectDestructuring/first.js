439bfea4ff2638b7ec2c834ab77926c774b4b60c
George <admin@georgesimos.com>
2020-02-26 13:17:10 +0200

    object default fallback

--- objectDestructuring/first.js
+++ objectDestructuring/first.js
@@ -1,8 +1,8 @@
 function data() {
-  return { b: 2, c: 3, d: 4 };
+  return { a: 1, b: 2, c: 3, d: 4 };
 }
 
-var tmp = data();
+var tmp = data() || {};
 var first = tmp.a !== undefined ? tmp.a : 42;
 var second = tmp.b;
 var third = tmp.c;
--- objectDestructuring/second.js
+++ objectDestructuring/second.js
@@ -1,6 +1,6 @@
 function data() {
-  return { b: 2, c: 3, d: 4 };
+  return { a: 1, b: 2, c: 3, d: 4 };
 }
-var tmp = data();
-var { a: first = 42, b: second, ...rest } = tmp;
+
+var { a: first = 42, b: second, ...rest } = data() || {};
 console.log(first, second, rest);
